9e18d1fef4583b45b7b68cd175b417625d615d9e
felixmaina <karani.maina2010@gmail.com>
2022-09-13 19:05:20 +0300

    create aget All books endpoint and return an array of  books

--- src/main/java/com/example/books/controller/BooksController.java
+++ src/main/java/com/example/books/controller/BooksController.java
@@ -1,9 +1,22 @@
 package com.example.books.controller;
 
+import com.example.books.models.Books;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 
 public class  BooksController{
-
+@GetMapping("/books")
+    public List<Books>getAllBooks(){
+    return Arrays.asList(
+            new Books(1L,"hero","felix"),
+            new Books(2L,"negro","drake"),
+            new Books(3L,"creation","Genevieve"),
+            new Books(4L,"maurice","maina")
+    );
+}
 }
