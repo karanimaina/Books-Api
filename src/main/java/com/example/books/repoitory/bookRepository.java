dd83eccee85b5200fa6fb8776056233788816cb5
felixmaina <karani.maina2010@gmail.com>
2022-09-14 11:29:29 +0300

    generate jdbc template that queries the object

--- src/main/java/com/example/books/repoitory/bookRepository.java
+++ src/main/java/com/example/books/repoitory/bookRepository.java
@@ -1,6 +1,7 @@
 package com.example.books.repoitory;
 
 import com.example.books.models.Book;
+import org.hibernate.dialect.LobMergeStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -12,8 +13,11 @@ import java.util.List;
 public class bookRepository {
     @Autowired
     JdbcTemplate jdbcTemplate;
-    //using the jdbc template from the sspting jdbc
+    //using the jdbc template from the spring jdbc
     public List<Book>findAll(){
         return  jdbcTemplate.query("select * from books", new BeanPropertyRowMapper<Book>(Book.class));
+    }
+    public Book findById(Long id){
+        return jdbcTemplate.queryForObject("select * from book where id =?",new BeanPropertyRowMapper<Book>(Book.class));
     }
 }
